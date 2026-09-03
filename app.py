e0081cf95ce3ec8ceedf5c9b389d19b7bd6e1cb1
rakkasimahe27-crypto <rakkasimahe27@gamil.com>
2026-09-03 19:22:00 +0530

    hello

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-# Text (String)
-user_name = "Alice"  
-
-# Numbers (Integer & Float)
-user_age = 30  
-<<<<<<< HEAD
-account_balance = 50.75  
-=======
-account_balance = 50.55  
->>>>>>> 65133f3e8d341e5675dcc8f495c7dc2ec9584b7c
-
-# Boolean (True/False)
-is_active = True  
-
+iidd
